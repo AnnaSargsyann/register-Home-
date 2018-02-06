e654b0c791cf49aab93d5a2c608d39ffa5d910c8
AnnaSargsyann <36159610+AnnaSargsyann@users.noreply.github.com>
2018-02-07 01:13:48 +0400

    Update Info.js

--- screens/Info.js
+++ screens/Info.js
@@ -1,72 +1,104 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+
 export default class Regform extends React.Component {
   render() {
     return(
 
       <View style={styles.regInfo}>
-      <View style={styles.regform_name}>
-        <View style={styles.namee}>
-          <Text>First name</Text>
-          <TextInput style={styles.textinput} placeholder="enter first name" underlineColorAndroid={'transparent'}/>
+
+        <View style={styles.for_info}>
+          <View style={styles.first}>
+            <Text>First Name</Text>
+            <TextInput style={styles.textinput} placeholder="enter first name" underlineColorAndroid={'transparent'}/>
+          </View>
+          <View style={styles.first}>
+            <Text>Last Name</Text>
+            <TextInput style={styles.textinput} placeholder="enter last name" underlineColorAndroid={'transparent'}/>
+          </View>
         </View>
-        <View style={styles.lastnamee}>
-          <Text>Last name</Text>
-          <TextInput style={styles.textinput} placeholder="enter last name" underlineColorAndroid={'transparent'}/>
+        <View style={styles.n}>
+        <Text>Mobile Number</Text>
+          <View style={styles.forNumber}>
+
+              <TextInput style={styles.textinput1} placeholder="+" underlineColorAndroid={'transparent'} />
+
+              <TextInput style={styles.textinput2} placeholder="enter mobile number" underlineColorAndroid={'transparent'}/>
+          </View>
         </View>
-        </View>
-        <View style={styles.lastname}>
-          <Text>Mobile number</Text>
-          <TextInput style={styles.number} placeholder="+" underlineColorAndroid={'transparent'}/>
-          <TextInput style={styles.num} placeholder="enter enter mobile number" underlineColorAndroid={'transparent'}/>
-        </View>
-        <View style={styles.lastname}>
-          <Text>Emaill Adress</Text>
-          <TextInput style={styles.textinput} placeholder="enter emaill adress" underlineColorAndroid={'transparent'}/>
-        </View>
-        <View style={styles.lastname}>
-          <Text>Password</Text>
-          <TextInput style={styles.textinput} placeholder="enter a Password" underlineColorAndroid={'transparent'}/>
-        </View>
-        <TouchableOpacity style={styles.button}>
+          <View style={styles.emaill}>
+            <Text>Emaill Adress</Text>
+            <TextInput style={styles.textinput} placeholder="enter emaill adress" underlineColorAndroid={'transparent'}/>
+          </View>
+          <View style={styles.password}>
+            <Text>Password</Text>
+            <TextInput style={styles.textinput} placeholder="enter a password" underlineColorAndroid={'transparent'}/>
+          </View>
+          <TouchableOpacity style={styles.button}>
         <Text style={styles.btText}>REGISTER</Text>
-        </TouchableOpacity>
-      </View>
+</TouchableOpacity>
+
+
+
+        </View>
 
     );
   }
 }
 const styles = StyleSheet.create({
-  regInfo:{
-    alignSelf:'stretch',
+  regInfo: {
+    paddingLeft:60,
+    paddingRight:60,
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  first: {
+    width:100,
+
+  },
+  for_info: {
+    flexDirection: "row",
+     justifyContent: "space-around",
+
+     borderColor:'green',
+  },
+  forNumber:{
+
+    flexDirection: "row",
+    justifyContent: "space-around",
+
+    borderColor:'green'
+  },
+  n:{
+    borderColor:'red',
+
+
   },
   Text:{
-    textDecorationStyle:'solid'
-  },
-  regform_name:{
-    flex:1,
-    flexDirection: 'row',
-    justifyContent: "space-around",
-},
-  namee:{
-    width: 100,
-    height: 100,
-},
-  lastnamee:{
-    width: 100, height: 100,
-},
-  textinput:{
-    borderBottomWidth: 1,
     marginBottom: 30,
-},
-number:{
+  },
+textinput: {
   borderBottomWidth: 1,
   marginBottom: 30,
-  width: 40,
 },
-num:{
-  marginBottom: 30,
+textinput1:{
   borderBottomWidth: 1,
+  width: 50,
+  marginBottom: 30,
+},
+textinput2:{
+  borderBottomWidth: 1,
+  width: 150,
+  marginBottom: 30,
+},
+emaill:{
+
+  borderColor:'green'
+},
+password:{
+
+  borderColor:'green'
 },
 button: {
   alignSelf:'stretch',
@@ -76,4 +108,6 @@ button: {
   padding: 20,
   color: 'white'
 }
+
+
 });
