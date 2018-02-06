5714c3f796185c83cb3aa6c6d5e92f2fe027dcd7
AnnaSargsyann <36159610+AnnaSargsyann@users.noreply.github.com>
2018-02-07 01:13:08 +0400

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      
+      <View style={styles.circle}></View>
       <Regform style={styles.reg}/>
       </View>
     );
@@ -16,15 +16,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'center',
+
     backgroundColor: '#fff',
-    paddingLeft:60,
-    paddingRight:60,
+
 
 
   },
+  circle: {
+    backgroundColor:'#6A1B9A',
 
+    height:100,
+    width:'100%',
+
+
+  },
   reg: {
-    marginTop: 500
+margin:300
+
   }
 });
